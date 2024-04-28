13a4de0f12cf982b0b288f147c7ce8e2db4b8323
John Holzer <jtholzer@outlook.com>
2024-04-28 15:29:57 -0700

    changed partioning in auto config

--- install/auto/default.nix
+++ install/auto/default.nix
@@ -26,31 +26,60 @@
 						done
 					}
 
-					dev=/dev/sda
-					[ -b /dev/nvme0n1 ] && dev=/dev/nvme0n1
-					[ -b /dev/vda ] && dev=/dev/vda
 
-					${utillinux}/bin/sfdisk --wipe=always $dev <<-END
-						label: gpt
 
-						name=BOOT, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-						name=NIXOS
-					END
-					mkfs.fat -F 32 -n BOOT /dev/disk/by-partlabel/BOOT
+					DEVICE="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"
+					if [ ! -b "$DEVICE" ]; then
+						echo "ERROR: The default disk $DEVICE is missing!"
+						exit 1;
+					fi
 
-					sync
-					wait-for [ -b /dev/disk/by-partlabel/BOOT ]
+					# List partitions on the device
+					PARTITIONS=$(lsblk "$DEVICE" --output NAME --noheadings --raw | wc -l)
 
-					wait-for mkfs.fat -F 32 -n BOOT /dev/disk/by-partlabel/BOOT
+					# Check if partitions exist
+					if [ "$PARTITIONS" != 1 ]; then
+							echo "Looks like the disk partitions are already setup, skipping this step!"
+					else
+							parted $DEVICE -- mklabel msdos
+							parted $DEVICE -- mkpart primary 1MB -8GB
+							parted $DEVICE -- mkpart primary linux-swap -8GB 100%
+							
+							mkfs.ext4 -L nixos $DEVICE-part1
+							mkswap -L swap $DEVICE-part2
+							swapon /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
+							mount /dev/disk/by-label/nixos /mnt
+					fi
 
-					wait-for [ -b /dev/disk/by-partlabel/NIXOS ]
-					mkfs.ext4 -L nixos /dev/mapper/root
+					# nixos-generate-config --root /mnt
 
-					sync
-					mount /dev/mapper/root /mnt
 
-					mkdir /mnt/boot
-					wait-for mount /dev/disk/by-label/boot /mnt/boot
+
+					# dev=/dev/sda
+					# [ -b /dev/nvme0n1 ] && dev=/dev/nvme0n1
+					# [ -b /dev/vda ] && dev=/dev/vda
+
+					# ${utillinux}/bin/sfdisk --wipe=always $dev <<-END
+					# 	label: gpt
+
+					# 	name=BOOT, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+					# 	name=NIXOS
+					# END
+					# mkfs.fat -F 32 -n BOOT /dev/disk/by-partlabel/BOOT
+
+					# sync
+					# wait-for [ -b /dev/disk/by-partlabel/BOOT ]
+
+					# wait-for mkfs.fat -F 32 -n BOOT /dev/disk/by-partlabel/BOOT
+
+					# wait-for [ -b /dev/disk/by-partlabel/NIXOS ]
+					# mkfs.ext4 -L nixos /dev/mapper/root
+
+					# sync
+					# mount /dev/mapper/root /mnt
+
+					# mkdir /mnt/boot
+					# wait-for mount /dev/disk/by-label/boot /mnt/boot
 
 					install -D ${./configuration.nix} /mnt/etc/nixos/configuration.nix
 					install -D ${./hardware-configuration.nix} /mnt/etc/nixos/hardware-configuration.nix
