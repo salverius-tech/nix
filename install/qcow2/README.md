1839f76488536693712368369776675e7231162e
John Holzer <jtholzer@outlook.com>
2024-05-01 15:45:18 -0700

    added qcow2 creation configs

---
+++ install/qcow2/README.md
@@ -0,0 +1,5 @@
+# To build the qcow2 image
+
+```
+nix build .#nixosConfigurations.build-qcow2.config.system.build.qcow2
+```
---
+++ install/qcow2/make-disk-image.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, modulesPath, ... }: 
+
+{
+
+  imports = [
+    "${toString modulesPath}/profiles/qemu-guest.nix"
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    autoResize = true;
+    fsType = "ext4";
+  };
+
+  boot.growPartition = true;
+  boot.kernelParams = ["console=ttyS0"];
+  boot.loader.grub.device = lib.mkDefault "/dev/vda";
+  boot.loader.timeout = 0;
+
+  system.build.qcow2 = import "${modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    diskSize = 10240;
+    format = "qcow2";
+    partitionTableType = "hybrid";
+  };
+
+}
